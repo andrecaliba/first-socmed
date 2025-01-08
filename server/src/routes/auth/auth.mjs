042f938fe46d5b2d4db497f0d909c755deeca09f
VerxMaxum <138268061+VerxMaxum@users.noreply.github.com>
2025-01-08 22:46:07 +0800

    Remove console logs

--- server/src/routes/auth/auth.mjs
+++ server/src/routes/auth/auth.mjs
@@ -3,9 +3,6 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/api/check-auth', (req, res) => {
-  console.log("Session: ", req.session);
-  console.log("Session user: ", req.session.user);
-  console.log("Session visited: ", req.session.visited);
   if (req.session.user) {
     res.json({ authenticated: true });
   } else {
